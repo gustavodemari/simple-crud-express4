5d1d7f1aee205d2943096711d75e41e360bd1cdd
gustavodemari <guhdemari@gmail.com>
2014-05-07 18:50:27 -0300

    Adding JSHint

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ var app = express();
 app.use(bodyParser());
 
 //Logging the request method, url and IP address
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
   console.log('%s %s %s', req.method, req.url, req.ip);
   next();
 });
 
 //Default url
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.send('hello world');
 });
 
@@ -22,7 +22,7 @@ app.route('/api/users')
   .get(user.read)
   .post(user.create)
   .put(user.update)
-  .delete(user.delete)
+  .delete(user.delete);
 
 var port = Number(process.env.PORT || 5000);
 
--- routes/user.js
+++ routes/user.js
@@ -1,35 +1,35 @@
 //Array to store users
 var users = [];
 
-exports.printMethodUrl = function(req, res){
-  res.json(req.method+' '+req.url);
+exports.printMethodUrl = function (req, res) {
+  res.json(req.method + ' ' + req.url);
 };
 
 //Creating user method
-exports.create = function(req, res){
+exports.create = function (req, res) {
   var user = req.body.user;
-  if(user){
+  if (user) {
     user.id = parseInt(user.id);
     users.push(user);
     res.json(200, {message: 'User created!'});
   }
-  else{
+  else {
     res.json(400, {message: 'Invalid input, try again'});
   }
-}
+};
 
 //Reading users array method
-exports.read = function(req, res){
+exports.read = function (req, res) {
   res.json(200, JSON.stringify(users));
 };
 
 //Update user info method
-exports.update = function(req, res){
+exports.update = function (req, res) {
   var user = req.body.user;
-  if(user){
+  if (user) {
     user.id = parseInt(user.id);
-    for(var i = 0; i < users.length; i++){
-      if(users[i].id===user.id){
+    for (var i = 0; i < users.length; i++) {
+      if (users[i].id === user.id) {
         users[i] = user;
         res.json(200, {message: 'User updated!'});
       }
@@ -42,15 +42,15 @@ exports.update = function(req, res){
 };
 
 //Delete user method
-exports.delete = function(req, res){
+exports.delete = function (req, res) {
   var user = req.body.user;
-  if(user){
+  if (user) {
     user.id = parseInt(user.id);
-    for(var i = 0; i < users.length; i++){
-      if(users[i].id===user.id){
+    for (var i = 0; i < users.length; i++) {
+      if (users[i].id === user.id) {
         users.splice(i, 1);
         res.json(200, {message: 'User deleted!'});
-      } 
+      }
     }
     res.json(400, {message: 'User not found'});
   }
